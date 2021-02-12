4f3850dc70fe3d5a942b6cec442b609101613fa5
kalebpulley <pul21001@byui.edu>
2021-02-11 20:02:25 -0700

    simple calculator

---
+++ js/simpleCalculator.js
@@ -0,0 +1,42 @@
+/*
+Input: get the values of the user's numbers operator and guess.
+Computation: compute the numbers acording to the operator using a switch statment.
+compare the answer with thier guess.
+Output: if the guess is right return Correct! 🏆 otherwise return Incorrect 😢
+*/
+function simpleCalculator() {
+
+    //input in these 4 lines
+    const fisrtNumber = parseFloat(document.getElementById('firstNumber').value);
+    let operator = document.getElementById('operator').value;
+    const sencondNumber = parseFloat(document.getElementById('sencondNumber').value);
+    const guess = parseFloat(document.getElementById('guess').value);
+    //Computation check what the answer should be.
+    let answer
+    switch (operator) {
+        case "+":
+            answer = fisrtNumber + sencondNumber;
+            break;
+        case "-":
+            answer = fisrtNumber - sencondNumber;
+            break;
+        case "*":
+            answer = fisrtNumber * sencondNumber;
+            break;
+        case "/":
+            answer = fisrtNumber / sencondNumber;
+            let digits = 1;
+            let multiplier = Math.pow(10, digits);
+            answer = Math.round(answer * multiplier) / multiplier;
+            break;
+    }
+    let output = ""    
+    //Output
+    if (answer == guess) {
+        output = "Correct! 🏆";
+    }else {
+        output = "Incorrect 😢";
+    }
+    
+    document.getElementById('output').innerHTML = output;  
+}
